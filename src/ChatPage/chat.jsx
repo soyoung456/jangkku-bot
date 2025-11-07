f66eb7275b5cd70f5466f64164b77c0f0e1fa72c
soyoung456 <147dudth@naver.com>
2025-11-07 21:55:45 +0900

    feat: 채팅 페이지 구현

---
+++ src/ChatPage/chat.jsx
@@ -0,0 +1,10 @@
+import ChatContainer from "./ChatContainer";
+import "./chat.css";
+
+export default function ChatPage() {
+  return (
+    <div className="chat-page">
+      <ChatContainer />
+    </div>
+  );
+}
